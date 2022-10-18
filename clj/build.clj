3ba39707ef994ccc5f149fc739dad27880370a1b
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-10-18 20:14:51 -0300

    Add comment

--- clj/build.clj
+++ clj/build.clj
@@ -3,9 +3,12 @@
 
 ;; Compiler options:
 ;; :output-to = define the name of the output file
-;; :main = tells the namespace of main goog compiler function to it be loaded
+;; - :main = tells the namespace of main goog compiler function to it be loaded
+;; :optimizations = optimizations for smaller builds. many options, consult on wiki
+;;                  in any optimized build all files are bundled in compiled.js so
+;;                  no need to define :main anymore.
 (def compiler-options {:output-to "compiled.js"
-                       :main 'demo.main})
+                       :optimizations :whitespace})
 
 ;; Compiler build function
 ;; this function compile the ClojureScript source to JS.
