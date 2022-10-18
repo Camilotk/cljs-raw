d42ac8731d5ae00214382d0b105233d75828850c
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-10-18 20:28:23 -0300

    Change the optimization level from whitespace to simple

--- clj/build.clj
+++ clj/build.clj
@@ -3,7 +3,6 @@
 
 ;; Compiler options:
 ;; :output-to = define the name of the output file
-;; - :main = tells the namespace of main goog compiler function to it be loaded
 ;; :optimizations = optimizations for smaller builds. many options, consult on wiki
 ;;                  in any optimized build all files are bundled in compiled.js so
 ;;                  no need to define :main anymore.
@@ -13,7 +12,7 @@
 (def compiler-options {:output-to "compiled.js"
                        :source-map "compiled.js.map"
                        :output-dir "out"
-                       :optimizations :whitespace})
+                       :optimizations :simple})
 
 ;; Compiler build function
 ;; this function compile the ClojureScript source to JS.
